4e37197d74b3de941c84de63cc3dcf80d53b71cb
Ernesto Cervantes <netocervantes@macbook-pro-de-ernesto.local>
2016-08-17 13:42:28 -0500

    Initial Commit

---
+++ PruebaGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PruebaGit
+//
+//  Created by Ernesto Cervantes on 17/08/16.
+//  Copyright © 2016 Ernesto Cervantes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PruebaGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PruebaGit
+//
+//  Created by Ernesto Cervantes on 17/08/16.
+//  Copyright © 2016 Ernesto Cervantes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
